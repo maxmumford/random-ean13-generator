469e319ca479875caff9145027301e241e8e29bf
Max Mumford <maxmumford@gmail.com>
2014-08-14 11:54:09 +0200

    [ADD] ean13 generator

---
+++ ean13-generator.py
@@ -0,0 +1,31 @@
+#! /usr/bin/python
+
+"""
+This script generates a random EAN13 number and prints it to the standard out.
+"""
+
+from random import randrange
+
+def generate_12_random_numbers():
+    numbers = []
+    for x in range(12):
+        numbers.append(randrange(10))
+    return numbers
+
+def calculate_checksum(ean):
+    """
+    Calculates the checksum for an EAN13
+    @param list ean: List of 12 numbers for first part of EAN13
+    :returns: The checksum for `ean`.
+    :rtype: Integer
+    """
+    assert len(ean) == 12, "EAN must be a list of 12 numbers"
+    sum_ = lambda x, y: int(x) + int(y)
+    evensum = reduce(sum_, ean[::2])
+    oddsum = reduce(sum_, ean[1::2])
+    return (10 - ((evensum + oddsum * 3) % 10)) % 10
+
+numbers = generate_12_random_numbers()
+numbers.append(calculate_checksum(numbers))
+print ''.join(map(str, numbers))
+
